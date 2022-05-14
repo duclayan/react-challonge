e1f60166c4c4b0571eb06fda89bc970ca4ea289a
Duclayan <duclayandominique@gmail.com>
2022-05-14 23:04:16 +0200

    Add create tournament with basic title, description and type

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import React, { Component } from "react";
 import TournamentColumn from "./components/tournaments/tournaments";
 import CallToActionWithAnnotation from "./components/Heading/Header";
 import WithSubnavigation from "./components/Heading/Navbar";
+import CreateTournament from "./components/tournaments/createTournament";
 const apiKey = `8KKWQ4LPxEjTdW0FRpZj6t87z0yjnyDquMjiaqGY`;
 const userName = `duclayan`;
 
-class App extends Component {
-  render() {
+function App(){
     return (
       <ChakraProvider>
         <div>
           <WithSubnavigation/>
           <CallToActionWithAnnotation />
         </div> 
+        <CreateTournament/>
         <TournamentColumn/>
       </ChakraProvider>
     );
-  }
 }
 
 export default App;
--- src/components/tournaments/createTournament.js
+++ src/components/tournaments/createTournament.js
@@ -1,151 +1,82 @@
 import React, { Component, useState } from "react";
-import { Heading, Container, Button, Input, FormLabel } from "@chakra-ui/react";
+import { Heading, Container, Button, Input, FormLabel, Select } from "@chakra-ui/react";
 import FormSubHeading from "./forms/formSubheading";
 import { RegularInput } from "./forms/regularInput";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
-export function CreateTournament(props) {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors, isSubmitting },
-  } = useForm();
+export default class CreateTournament extends React.Component {
 
-  const [tournament_name, setName] = useState("");
-  const [tournament_description, setDescription] = useState("");
-
-function onSubmit(e) {
-
-    const apiUrl = "https://api.challonge.com/v1/tournaments."
-    const tournament = {
-        "name": tournament_name, 
-        "description": tournament_description
+    state = {
+        name: '',
+        description: '',
+        tournament_type: ''
     }
 
-    const sample_data = {
-        "tournament": {
-          "accept_attachments": false,
-          "allow_participant_match_reporting": true,
-          "anonymous_voting": false,
-          "category": null,
-          "check_in_duration": null,
-          "completed_at": null,
-          "created_at": "2015-01-19T16:47:30-05:00",
-          "created_by_api": false,
-          "credit_capped": false,
-          "description": "",
-          "game_id": 600,
-          "group_stages_enabled": false,
-          "hide_forum": false,
-          "hide_seeds": false,
-          "hold_third_place_match": false,
-          "id": 1086875,
-          "max_predictions_per_user": 1,
-          "name": "Sample Tournament 1",
-          "notify_users_when_matches_open": true,
-          "notify_users_when_the_tournament_ends": true,
-          "open_signup": false,
-          "participants_count": 4,
-          "prediction_method": 0,
-          "predictions_opened_at": null,
-          "private": false,
-          "progress_meter": 0,
-          "pts_for_bye": "1.0",
-          "pts_for_game_tie": "0.0",
-          "pts_for_game_win": "0.0",
-          "pts_for_match_tie": "0.5",
-          "pts_for_match_win": "1.0",
-          "quick_advance": false,
-          "ranked_by": "match wins",
-          "require_score_agreement": false,
-          "rr_pts_for_game_tie": "0.0",
-          "rr_pts_for_game_win": "0.0",
-          "rr_pts_for_match_tie": "0.5",
-          "rr_pts_for_match_win": "1.0",
-          "sequential_pairings": false,
-          "show_rounds": true,
-          "signup_cap": null,
-          "start_at": null,
-          "started_at": null,
-          "started_checking_in_at": null,
-          "state": "pending",
-          "swiss_rounds": 0,
-          "teams": false,
-          "tie_breaks": [
-            "match wins vs tied",
-            "game wins",
-            "points scored"
-          ],
-          "tournament_type": "single elimination",
-          "updated_at": "2015-01-19T16:57:17-05:00",
-          "url": "sample_tournament_1",
-          "description_source": "",
-          "subdomain": null,
-          "full_challonge_url": "http://challonge.com/sample_tournament_1",
-          "live_image_url": "http://images.challonge.com/sample_tournament_1.png",
-          "sign_up_url": null,
-          "review_before_finalizing": true,
-          "accepting_predictions": false,
-          "participants_locked": true,
-          "game_name": "Table Tennis",
-          "participants_swappable": false,
-          "team_convertable": false,
-          "group_stages_were_started": false
+    handleChange = (event, key) => {
+        const { name, value } = event.target;
+        console.log('handle change')
+        console.log('key', name,value)
+        this.setState({ [name]: value})
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const tournament = {
+            name: this.state.name,
+            description: this.state.description,
+            tournament_type: this.state.tournament_type
         }
-      }
-    axios.post(apiUrl,
-        {data: {
-            "tournament[name]":`${tournament_name}`,
-            "tournament[description]":`${tournament_description}`
-        }},
-        {headers:{
-            "content-type":'application/json',
-            "Accept":'application/json'
-        }}
-    )
+
+        axios.post("https://api.challonge.com/v1/tournaments.json",{tournament})
+            .then( res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
+
+render(){
+    return (
+        <>
+          <Container maxW={"3xl"}>
+            <form onSubmit={this.handleSubmit}>
+              <Heading color={"green.400"} fontSize="50px">
+                New Tournament
+              </Heading>
     
-    .then(function(response){
-        console.log(response)
-    })
-    .catch(function(error){
-        console.log(error)
-        console.log('error data', error.response)
-    })
-    console.log("on Submit",tournament_description);
-  }
+              <FormSubHeading title="Basic Info" />
+    
+              <FormLabel htmlFor="tournament-name">Tournament Name</FormLabel>
+              <Input
+                id="tournament-name"
+                placeholder="Tournament Name"
+                name="name"
+                onChange={this.handleChange}
+              />
+    
+                <FormLabel htmlFor="tournament-description">Tournament Description</FormLabel>
+                <Input
+                    id="tournament-description"
+                    placeholder="tournament description"
+                    name="description"
+                    onChange={this.handleChange}
+                />
 
-
-  return (
-    <>
-      <Container maxW={"3xl"}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Heading color={"green.400"} fontSize="50px">
-            New Tournament
-          </Heading>
-
-          <FormSubHeading title="Basic Info" />
-
-          <FormLabel htmlFor="tournament-name">Tournament Name</FormLabel>
-          <Input
-            id="tournament-name"
-            placeholder="Tournament Name"
-            onChange={(e) => setName(e.target.value)}
-          />
-
-<FormLabel htmlFor="tournament-description">Tournament Description</FormLabel>
-          <Input
-            id="tournament-description"
-            placeholder="tournament description"
-            onChange={(e) => setDescription(e.target.value)}
-          />
-
-          <Button mt={4} colorScheme="teal" type="submit">
-            Submit
-          </Button>
-        </form>
-      </Container>
-    </>
-  );
-}
-export default CreateTournament;
+                <FormLabel htmlFor="tournament-description">Format </FormLabel>
+                <Select placeholder='Select option'  name="tournament_type" onChange={this.handleChange}>
+                <option value='single elimination'>Single Elimination</option>
+                <option value='double elimination'>Double Elimination</option>
+                <option value='round robin'>Round Robin</option>
+                <option value='swiss'>Swiss</option>
+                <option value='free for all'>Free for all</option>
+                <option value='leaderboard'>Leaderboard</option>
+                </Select>
+              <Button mt={4} colorScheme="teal" type="submit">
+                Submit
+              </Button>
+            </form>
+          </Container>
+        </>
+      );
+}}
