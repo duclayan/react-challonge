29a9396cd954c286d7757fbc8f91491f6341e4be
Duclayan <duclayandominique@gmail.com>
2022-05-16 02:00:41 +0200

    Fix path of create account

--- src/components/Heading/Navbar.js
+++ src/components/Heading/Navbar.js
@@ -86,7 +86,7 @@ export default function WithSubnavigation() {
 
           <Link
             to={{
-              pathname: "/tournaments",
+              pathname: "/tournament/create",
             }}
           >
             <Button
